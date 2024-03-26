92cdf35a32e2c60f9c5d67ad5a0401f50a76a22b
djembuka <djembuka@gmail.com>
2024-03-26 16:54:57 +0300

    Dates controls close

--- src/components/controls/ControlDateSingle.vue
+++ src/components/controls/ControlDateSingle.vue
@@ -322,12 +322,8 @@ export default {
 .twpx-form-control--date .dp__input_icon {
   display: none;
 }
-
-/*
-.twpx-form-control--date .dp__clear_icon path {
-  display: none;
-}
 .twpx-form-control--date .dp__clear_icon {
+  padding: 0;
   position: absolute;
   top: 16px;
   right: 16px;
@@ -335,32 +331,15 @@ export default {
   height: 16px;
   cursor: pointer;
   transition: opacity 0.5s ease;
-  padding: 0;
   z-index: 10;
+}
+
+.twpx-form-control--date .dp__clear_icon {
+  width: 26px;
+  height: 26px;
+  top: 50%;
 }
 .twpx-form-control--date .dp__clear_icon:hover {
   opacity: 0.7;
 }
-.twpx-form-control--date .dp__clear_icon:before {
-  content: '';
-  display: block;
-  height: 20px;
-  border-left: 2px solid #000;
-  transform: rotate(45deg);
-  position: absolute;
-  top: -2px;
-  left: 7px;
-  z-index: 10;
-}
-.twpx-form-control--date .dp__clear_icon:after {
-  content: '';
-  display: block;
-  height: 20px;
-  border-left: 2px solid #000;
-  transform: rotate(135deg);
-  position: absolute;
-  top: -2px;
-  left: 7px;
-  z-index: 10;
-}*/
 </style>
